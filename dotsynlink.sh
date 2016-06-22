8bd90a50f2a0153e3858e186e1b5cf4e4e543768
steav <steave6@gmail.com>
2016-06-23 02:06:05 +0900

    modified for array of files in current directory

--- dotsynlink.sh
+++ dotsynlink.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-DOT_FILES=(.bashrc .bash_profile .vimrc .profile .xbindkeysrc .Xmodmap .gitconfig bin)
+# DOT_FILES=(.bashrc .bash_profile .vimrc .profile .xbindkeysrc .Xmodmap .gitconfig bin)
 
+DOT_FILES=$( ls -A | grep -Ev '^.git$|*.sh$' )
+echo $DOT_FILES
 
-for file in ${DOT_FILES[@]}
-do
-  ln -s $HOME/dotfiles/$file $HOME/$file
-done
+# for file in ${DOT_FILES[@]}
+# do
+#   ln -s $HOME/dotfiles/$file $HOME/$file
+# done
